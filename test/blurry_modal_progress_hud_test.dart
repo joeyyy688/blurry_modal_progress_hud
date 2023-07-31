3747da306290ce82da30d5aa960158350d28c1f1
joeyyy688 <edinjoey@gmail.com>
2023-07-31 20:06:42 +0000

    Added tests

--- test/blurry_modal_progress_hud_test.dart
+++ test/blurry_modal_progress_hud_test.dart
@@ -1,10 +1,56 @@
+import 'package:blurry_modal_progress_hud/src/blurry_modal_progress_hud.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+  testWidgets('Child widget should be rendered when inAsyncCall is false',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: BlurryModalProgressHUD(
+          inAsyncCall: false,
+          child: Text('Test Child Widget'),
+        ),
+      ),
+    );
+
+    expect(find.text('Test Child Widget'), findsOneWidget);
+  });
+
+  testWidgets('Progress indicator should be shown when inAsyncCall is true',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: BlurryModalProgressHUD(
+          inAsyncCall: true,
+          child: Text('Test Child Widget'),
+        ),
+      ),
+    );
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets(
+      'Progress indicator should be positioned correctly when offset is provided',
+      (WidgetTester tester) async {
+    const offset = Offset(100, 100);
+
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: BlurryModalProgressHUD(
+          inAsyncCall: true,
+          offset: offset,
+          child: Text('Test Child Widget'),
+        ),
+      ),
+    );
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    // Get the position of the progress indicator
+    final progressIndicator =
+        tester.getTopLeft(find.byType(CircularProgressIndicator));
+    expect(progressIndicator, offset);
   });
 }
